25e416fff4202fd421b71b9c67fd3bfe16a133c9
Muffin Cat <matanshemaya@gmail.com>
2023-01-06 18:30:12 +0200

    Update MailUtil.java

--- Mail/src/MailUtil.java
+++ Mail/src/MailUtil.java
@@ -12,10 +12,10 @@ public class MailUtil
 		System.out.println("Prepering to send email");
 		Properties properties = new Properties();
 		
-	    properties.put("mail.smtp.host","smtp.gmail.com");
-	    properties.put("mail.smtp.port", 587);
-	    properties.put("mail.smtp.auth", true);
-	    properties.put("mail.smtp.starttls.enable", "true");
+	    	properties.put("mail.smtp.host","smtp.gmail.com");
+	   	properties.put("mail.smtp.port", 587);
+	    	properties.put("mail.smtp.auth", true);
+	    	properties.put("mail.smtp.starttls.enable", "true");
 		
 		String myAccountEmail = "xxxxxx@gmail.com";
 		String password = "xxxxxxxx";
